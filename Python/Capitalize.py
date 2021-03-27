3de4d54d9fed662a3b9491a8ae2cff7c5cc95a8c
gudd78 <guddu78pinjarkar@gmail.com>
2021-03-27 23:21:41 +0530

    capitalised python

---
+++ Python/Capitalize.py
@@ -0,0 +1,12 @@
+def solve(s):
+    x=s.split()
+    for i in x:
+        s = s.replace(i, i.capitalize())
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = solve(s)
+    fptr.write(result + '\n')
+    fptr.close()
